1456dbeee269110381559876f0c6a906fd533772
RuslanG4 <ruslangavrilov118@gmail.com>
2025-03-04 16:57:28 +0000

    added loading async scenes and working animations

--- Packages/towerdefenceui.package/RunTime/Scripts/General/UIGeneralController.cs
+++ Packages/towerdefenceui.package/RunTime/Scripts/General/UIGeneralController.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections;
 using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIGeneralController : MonoBehaviour
 {
@@ -14,7 +17,7 @@ public class UIGeneralController : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject); // Prevent duplicate instances
+            Destroy(gameObject);
         }
     }
 
@@ -38,7 +41,11 @@ public class UIGeneralController : MonoBehaviour
     private IEnumerator AnimationCoroutine(string animation, Animator animator, TaskCompletionSource<bool> tcs)
     {
         animator.Play(animation);
+
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName(animation));
+
         yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1);
+
         tcs.SetResult(true);
     }
 
@@ -49,28 +56,58 @@ public class UIGeneralController : MonoBehaviour
             CanvasGroupController.EnableGroup(group);
             await _instance.WaitForAnimation(group, "Open");
         }
-        else
-        {
-            Debug.LogError("UIGeneralController instance is missing in the scene. Add it to a GameObject.");
-        }
     }
     public static async Task CloseUI(CanvasGroup group)
     {
         if (_instance != null)
         {
             await _instance.WaitForAnimation(group, "Close");
-            CanvasGroupController.DisableGroup(group);
-        }
-        else
-        {
-            Debug.LogError("UIController instance is missing in the scene!");
         }
     }
 
-    private async Task WaitForAnimation(CanvasGroup group, string animationName)
+    private async Task WaitForAnimation(CanvasGroup group, string name)
     {
-        await WaitForAnimationCoroutine(animationName, group.GetComponent<Animator>());
+        await WaitForAnimationCoroutine(name, group.GetComponent<Animator>());
     }
+
+    public static void LoadScene(CanvasGroup group, string scene)
+    {
+        if (scene != "")
+        {
+            _instance.StartCoroutine(_instance.LoadAsynchronously(scene, group));
+        }
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName, CanvasGroup group)
+    { 
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        CanvasGroupController.EnableGroup(group);
+
+        Slider loadingSlider = group.GetComponentInChildren<Slider>();
+        TMP_Text progressText = group.GetComponentInChildren<TMP_Text>();
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .95f);
+            loadingSlider.value = progress;
+
+            if (operation.progress >= 0.9f)
+            {
+                progressText.text = "Tap to continue";
+                loadingSlider.value = 1;
+
+                if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
 }
 
 
