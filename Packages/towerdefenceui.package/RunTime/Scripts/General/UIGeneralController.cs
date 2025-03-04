556711eb6c29f0c6a670f7d6227b818422ca608f
RuslanG4 <ruslangavrilov118@gmail.com>
2025-03-04 15:11:53 +0000

    Bring over Menu UI components #1
    changes to tasks
    
    fixing closing animations
    
    trying to update prefabs

---
+++ Packages/towerdefenceui.package/RunTime/Scripts/General/UIGeneralController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class UIGeneralController : MonoBehaviour
+{
+    private static UIGeneralController _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicate instances
+        }
+    }
+
+    public static void ToggleExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    private Task WaitForAnimationCoroutine(string animation, Animator animator)
+    {
+        var tcs = new TaskCompletionSource<bool>();
+
+        _instance.StartCoroutine(AnimationCoroutine(animation, animator, tcs));
+
+        return tcs.Task;
+    }
+
+    private IEnumerator AnimationCoroutine(string animation, Animator animator, TaskCompletionSource<bool> tcs)
+    {
+        animator.Play(animation);
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1);
+        tcs.SetResult(true);
+    }
+
+    public static async Task ToggleUI(CanvasGroup group)
+    {
+        CanvasGroupController.EnableGroup(group);
+
+        if (_instance != null)
+        {
+            await _instance.WaitForAnimation(group, "Open");
+        }
+        else
+        {
+            Debug.LogError("UIGeneralController instance is missing in the scene. Add it to a GameObject.");
+        }
+    }
+    public static async Task CloseUI(CanvasGroup group)
+    {
+        if (_instance != null)
+        {
+            await _instance.WaitForAnimation(group, "Close");
+        }
+        else
+        {
+            Debug.LogError("UIController instance is missing in the scene!");
+        }
+    }
+
+    private async Task WaitForAnimation(CanvasGroup group, string animationName)
+    {
+        await WaitForAnimationCoroutine(animationName, group.GetComponent<Animator>());
+        CanvasGroupController.DisableGroup(group);
+    }
+}
+
+
+
